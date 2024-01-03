8f484c567aef84338bf042b79aac848ee5a90d12
Alessandro Schivano <alessandro.schivano@gmail.com>
2024-01-03 14:47:17 +0100

    Aggiunto script per aggiornameno

---
+++ update.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Parametri
+wazuh_version="4.7.1"
+repository="https://github.com/wazuh/wazuh-docker.git"
+custom_config_url="https://github.com/AlexDroid00/wazuh-docker/raw/main/custom_config.zip"
+
+# Verifico se lo script è stato eseguito come root
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root"
+  exit 1
+fi
+# Verifica se unzip è installato
+if ! command -v unzip &> /dev/null; then
+    echo "unzip is not installed"
+    exit 1
+fi
+# Verifica se docker è installato
+if ! command -v docker &> /dev/null; then
+    echo "docker is not installed"
+    exit 1
+fi
+# Verifica se docker-compose è installato
+if ! command -v docker compose &> /dev/null; then
+    echo "docker compose is not installed"
+    exit 1
+fi
+
+# Spegno i container
+cd wazuh-docker/single-node/
+echo "Shutting down containers"
+docker compose down 2> /dev/null
+
+# Checkout sul nuovo branch
+cd ..
+git checkout v$wazuh_version
+
+# Riapplico il file di configurazione ossec.conf
+cd single-node/
+wget $custom_config_url
+mkdir custom_config
+unzip custom_config.zip -d custom_config
+if grep -q "./config/wazuh_cluster/wazuh_manager.conf:/wazuh-config-mount/etc/ossec.conf" docker-compose.yml; then
+    sed -i '/- \.\/config\/wazuh_cluster\/wazuh_manager.conf:\/wazuh-config-mount\/etc\/ossec.conf/d' docker-compose.yml # La configurazione si trova già nel volume wazuh_etc
+    mv config/wazuh_cluster/wazuh_manager.conf config/wazuh_cluster/wazuh_manager.conf.new # Per riferimenti futuri
+else
+    echo "I was unable to remove mounting of the ossec.conf file. Configuration may be incorrect."
+fi
+
+# Riavvio i container
+docker compose up -d
+
+# Pulizia
+rm -R custom_config/ custom_config.zip
